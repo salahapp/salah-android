5a5bf515baac704af5ef2f1224ade31673d7d29b
Faris Mohamed <faris@posteo.net>
2021-09-06 22:44:34 -0400

    Upgrade compose and compose icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,24 +44,22 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
-        kotlinCompilerVersion = "1.4.32"
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.3.2")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha07")
-    implementation("com.google.accompanist:accompanist-pager:0.8.1")
-    implementation("br.com.devsrsouza.compose.icons.android:tabler-icons:0.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.activity:activity-compose:${rootProject.extra["compose_version"]}")
+    implementation("com.google.accompanist:accompanist-pager:${rootProject.extra["accompanist_version"]}")
+    implementation("br.com.devsrsouza.compose.icons.android:tabler-icons:1.0.0")
     implementation("com.google.android.gms:play-services-location:18.0.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta05")
+    implementation("androidx.compose.runtime:runtime-livedata:${rootProject.extra["compose_version"]}")
     implementation("com.github.atutorme:kotlin-azan:0.0.9")
     implementation("com.karumi:dexter:6.2.2")
     testImplementation("junit:junit:4.+")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.0.0-beta05")
+    val compose_version by extra("1.0.2")
+    val accompanist_version by extra("0.18.0")
     repositories {
 
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha15")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+        classpath("com.android.tools.build:gradle:7.0.0-rc01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
